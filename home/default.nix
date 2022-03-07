f17fad18d33bb2a885fd1830e062b5666597aa31
ri7nz <ri7nz@evilfactory.id>
2022-03-07 17:18:30 +0700

    feat(home): symlink Applications Home when darwin

--- home/default.nix
+++ home/default.nix
@@ -12,4 +12,27 @@
     ./packages.nix
     ./shells.nix
   ];
+
+  home.activation = {
+    copyApplications =
+      let
+        apps = pkgs.buildEnv {
+          name = "home-manager-applications";
+          paths = config.home.packages;
+          pathsToLink = "/Applications";
+        };
+      in
+      lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        baseDir="$HOME/Applications/Home Manager Apps"
+        if [ -d "$baseDir" ]; then
+          rm -rf "$baseDir"
+        fi
+        mkdir -p "$baseDir"
+        for appFile in ${apps}/Applications/*; do
+          target="$baseDir/$(basename "$appFile")"
+          $DRY_RUN_CMD cp ''${VERBOSE_ARG:+-v} -fHRL "$appFile" "$baseDir"
+          $DRY_RUN_CMD chmod ''${VERBOSE_ARG:+-v} -R +w "$target"
+        done
+      '';
+  };
 }

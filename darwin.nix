e91bbd2a686102d7c835d057ad529562910d3805
ri7nz <ri7nz@evilfactory.id>
2022-03-13 01:54:32 +0700

    feat!(darwin): disable kitty

--- darwin.nix
+++ darwin.nix
@@ -75,7 +75,8 @@
 
   # https://github.com/nix-community/home-manager/issues/423
   environment.variables = {
-    TERMINFO_DIRS = "${pkgs.kitty.terminfo.outPath}/share/terminfo";
+    # 
+    # TERMINFO_DIRS = "${pkgs.kitty.terminfo.outPath}/share/terminfo";
   };
   programs.nix-index.enable = true;
 

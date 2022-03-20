aac73a202f8c00554b0c408067ce39fbf2c38859
ri7nz <ri7nz@evilfactory.id>
2022-03-21 02:51:42 +0700

    feat(packages): password-store extensions otp or totp

--- home/packages.nix
+++ home/packages.nix
@@ -25,98 +25,102 @@
   programs.htop.enable = true;
   programs.htop.settings.show_program_path = true;
 
+  programs.password-store.enable = true;
+  programs.password-store.package = pkgs.pass.withExtensions (exts: [ exts.pass-otp ]);
+
   # home.file.".gnupg/gpg-agent.conf" = {
   #   target = ".gnupg/gpg-agent.conf";
   #   text = ''
   #   pinentry-program ${pkgs.pinentry_mac}/pinentry-mac.app/Contents/MacOS/pinentry-mac
   # '';
   # };
-  home.packages = with pkgs; [
-    ################################## 
-    # common
-    ################################## 
-    coreutils
-    curl
-    wget
-    tree
-    gnupg # required for pass git
-    pass # password management
-    ack
+  home.packages = with pkgs;
+    [
+      ################################## 
+      # common
+      ################################## 
+      coreutils
+      curl
+      wget
+      tree
+      gnupg # required for pass git
+      # pass # password management
+      ack
 
-    ################################## 
-    # Platform specific
-    ################################## 
-    asciinema # screen record
-    glab # gitlab cli
-    gh # github cli
-    nodePackages.svg-term-cli
-    nodePackages."@napi-rs/cli"
-    nodePackages.mrm
+      ################################## 
+      # Platform specific
+      ################################## 
+      asciinema # screen record
+      glab # gitlab cli
+      gh # github cli
+      nodePackages.svg-term-cli
+      nodePackages."@napi-rs/cli"
+      nodePackages.mrm
 
-    ################################## 
-    # Manager
-    ################################## 
-    # yadm
+      ################################## 
+      # Manager
+      ################################## 
+      # yadm
 
-    ################################## 
-    # Productivity
-    ################################## 
-    fzf # finder
-    neofetch # fancy fetch information
-    du-dust # fancy du
-    fd # fancy find
-    jq # JSON in shell
-    ripgrep # another yet of grep
-    thefuck # hints command
-    ffmpeg
-    imagemagick
+      ################################## 
+      # Productivity
+      ################################## 
+      fzf # finder
+      neofetch # fancy fetch information
+      du-dust # fancy du
+      fd # fancy find
+      jq # JSON in shell
+      ripgrep # another yet of grep
+      thefuck # hints command
+      ffmpeg
+      imagemagick
 
-    ################################## 
-    # Development
-    ################################## 
-    git
-    neovim
-    yarn
-    tokei
-    rustPackages.rustc
-    rustPackages.rustfmt
-    rustPackages.cargo
-    google-cloud-sdk
-    nodejs-16_x
-    gitlab-runner
-    comby
-    python3
-    pkg-config
+      ################################## 
+      # Development
+      ################################## 
+      git
+      neovim
+      yarn
+      tokei
+      rustPackages.rustc
+      rustPackages.rustfmt
+      rustPackages.cargo
+      google-cloud-sdk
+      nodejs-16_x
+      gitlab-runner
+      comby
+      python3
+      pkg-config
 
-    ################################## 
-    # Shell Integrations
-    ################################## 
-    tmux # terminal multi-plexer (multiply terminal)
-    starship # theme for shell (bash,fish,zsh)
+      ################################## 
+      # Shell Integrations
+      ################################## 
+      tmux # terminal multi-plexer (multiply terminal)
+      starship # theme for shell (bash,fish,zsh)
 
-    ################################## 
-    # Misc
-    ################################## 
-    spotifyd # spotify daemon for TUI
-    spotify-tui # spotify terminal UI
+      ################################## 
+      # Misc
+      ################################## 
+      spotifyd # spotify daemon for TUI
+      spotify-tui # spotify terminal UI
 
-    ################################## 
-    # Communication
-    ################################## 
-    discord
-    slack
+      ################################## 
+      # Communication
+      ################################## 
+      discord
+      slack
 
-    ################################## 
-    # Useful Nix related tools
-    ################################## 
-    cachix
-    comma # run without install
-    nodePackages.node2nix
-    nix-prefetch-git
-  ] ++ lib.optionals stdenv.isDarwin [
-    cocoapods
-    m-cli # useful macOS CLI commands
-    pinentry_mac # gpg-agent for mac
-    xcode-install
-  ];
+      ################################## 
+      # Useful Nix related tools
+      ################################## 
+      cachix
+      comma # run without install
+      nodePackages.node2nix
+      nix-prefetch-git
+    ] ++ lib.optionals stdenv.isDarwin [
+      cocoapods
+      m-cli # useful macOS CLI commands
+      pinentry_mac # gpg-agent for mac
+      xcode-install
+    ];
 }

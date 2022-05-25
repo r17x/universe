0c4921ede26d7870c55002c21c45769c9c223c4c
ri7nz <ri7nz@evilfactory.id>
2022-05-26 01:48:09 +0700

    feat(darwin): disable yggdrasil

--- darwin.nix
+++ darwin.nix
@@ -70,7 +70,7 @@
   # `home-manager` currently has issues adding them to `~/Applications`
   # Issue: https://github.com/nix-community/home-manager/issues/1341
   environment.systemPackages = with pkgs; [
-    yggdrasil
+    # yggdrasil
     dnscrypt-proxy2
     terminal-notifier
   ];
@@ -111,62 +111,62 @@
     ];
   };
   # yggdrasil see https://yggdrasil-network.github.io/
-  launchd.agents.yggdrasil = {
-    serviceConfig.RunAtLoad = true;
-    serviceConfig.KeepAlive = true;
-    serviceConfig.ProcessType = "Interactive";
-    serviceConfig.StandardOutPath = "/tmp/yggdrasil.out.log";
-    serviceConfig.StandardErrorPath = "/tmp/yggdrasil.err.log";
-    serviceConfig.ProgramArguments = [
-      "${pkgs.yggdrasil}/bin/yggdrasil"
-      "-useconffile"
-      (toString (pkgs.writeText "yggdrasil.conf" ''
-            {
-        Peers: [
-          tls://yggdr.id:4433
-        ]
+  # launchd.agents.yggdrasil = {
+  #   serviceConfig.RunAtLoad = true;
+  #   serviceConfig.KeepAlive = true;
+  #   serviceConfig.ProcessType = "Interactive";
+  #   serviceConfig.StandardOutPath = "/tmp/yggdrasil.out.log";
+  #   serviceConfig.StandardErrorPath = "/tmp/yggdrasil.err.log";
+  #   serviceConfig.ProgramArguments = [
+  #     "${pkgs.yggdrasil}/bin/yggdrasil"
+  #     "-useconffile"
+  #     (toString (pkgs.writeText "yggdrasil.conf" ''
+  #           {
+  #       Peers: [
+  #         tls://yggdr.id:4433
+  #       ]
 
-        InterfacePeers: {}
+  #       InterfacePeers: {}
 
-        Listen: [
-          tls://0.0.0.0:0
-        ]
+  #       Listen: [
+  #         tls://0.0.0.0:0
+  #       ]
 
-        AdminListen: none
+  #       AdminListen: none
 
-        MulticastInterfaces:
-        [
-          {
-            Regex: en.*
-            Beacon: true
-            Listen: true
-            Port: 0
-          }
-          {
-            Regex: bridge.*
-            Beacon: true
-            Listen: true
-            Port: 0
-          }
-        ]
+  #       MulticastInterfaces:
+  #       [
+  #         {
+  #           Regex: en.*
+  #           Beacon: true
+  #           Listen: true
+  #           Port: 0
+  #         }
+  #         {
+  #           Regex: bridge.*
+  #           Beacon: true
+  #           Listen: true
+  #           Port: 0
+  #         }
+  #       ]
 
-        AllowedPublicKeys: []
+  #       AllowedPublicKeys: []
 
-        PublicKey: 22e1d2156e4984696caba8d95fa110e54efc09d1dee0e816d1011dd2d4dd5038
+  #       PublicKey: 22e1d2156e4984696caba8d95fa110e54efc09d1dee0e816d1011dd2d4dd5038
 
-        PrivateKey: ff95a9e5095e6324bd90632550b0b19b34629b4eecdb4b66646214f4ffe05eca22e1d2156e4984696caba8d95fa110e54efc09d1dee0e816d1011dd2d4dd5038
+  #       PrivateKey: ff95a9e5095e6324bd90632550b0b19b34629b4eecdb4b66646214f4ffe05eca22e1d2156e4984696caba8d95fa110e54efc09d1dee0e816d1011dd2d4dd5038
 
-        IfName: auto
+  #       IfName: auto
 
-        IfMTU: 65535
+  #       IfMTU: 65535
 
-        NodeInfoPrivacy: false
+  #       NodeInfoPrivacy: false
 
-        NodeInfo: {}
-            }
-      ''))
-    ];
-  };
+  #       NodeInfo: {}
+  #           }
+  #     ''))
+  #   ];
+  # };
 
 
 }

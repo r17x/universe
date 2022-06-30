6075f17e903c8adffd8e34cdec2a1d55363e87b6
r17x <ri7nz@evilfactory.id>
2022-07-01 04:21:17 +0700

    refactor(home): apply shellAliases in home
    all aliases can called in another shell (not just fish)

--- home/shells.nix
+++ home/shells.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, ... }:
+{ pkgs, lib, ... }:
 let
   shellAliases = with pkgs; {
     # Nix related
@@ -58,103 +58,99 @@ let
   };
 in
 {
-  # Fish Shell (Default shell)
-  # https://rycee.gitlab.io/home-manager/options.html#opt-programs.fish.enable
-  programs.fish.enable = true;
+  home = {
+    shellAliases = shellAliases; 
+    packages = with pkgs.fishPlugins;[
+      pkgs.thefuck
+      # https://github.com/franciscolourenco/done
+      done
+      # use babelfish than foreign-env
+      foreign-env
+      # https://github.com/wfxr/forgit
+      forgit
+      # Paired symbols in the command line
+      pisces
+    ];
+  }; 
 
-  # Fish plugins 
-  # See: 
-  # https://github.com/NixOS/nixpkgs/tree/90e20fc4559d57d33c302a6a1dce545b5b2a2a22/pkgs/shells/fish/plugins 
-  # for list available plugins built-in nixpkgs
-  home.packages = with pkgs.fishPlugins;[
-    pkgs.thefuck
-    # https://github.com/franciscolourenco/done
-    done
-    # use babelfish than foreign-env
-    foreign-env
-    # https://github.com/wfxr/forgit
-    forgit
-    # Paired symbols in the command line
-    pisces
-  ];
-
-
-  programs.fish.plugins = with pkgs.fishPlugins;[
-    {
-      name = "nix-env";
-      src = pkgs.fetchFromGitHub {
-        owner = "lilyball";
-        repo = "nix-env.fish";
-        rev = "7b65bd228429e852c8fdfa07601159130a818cfa";
-        sha256 = "069ybzdj29s320wzdyxqjhmpm9ir5815yx6n522adav0z2nz8vs4";
-      };
-    }
-    {
-      name = "thefuck";
-      src = pkgs.fetchFromGitHub
+  programs = {
+    # jump like `z` or `fasd` 
+    zoxide.enable = true;
+    dircolors.enable = true;
+    # Fish Shell (Default shell)
+    # https://rycee.gitlab.io/home-manager/options.html#opt-programs.fish.enable
+    fish = {
+      enable = true;
+      # Fish plugins 
+      # See: 
+      # https://github.com/NixOS/nixpkgs/tree/90e20fc4559d57d33c302a6a1dce545b5b2a2a22/pkgs/shells/fish/plugins 
+      # for list available plugins built-in nixpkgs
+      plugins = with pkgs.fishPlugins;[
         {
-          owner = "oh-my-fish";
-          repo = "plugin-thefuck";
-          rev = "6c9a926d045dc404a11854a645917b368f78fc4d";
-          sha256 = "1n6ibqcgsq1p8lblj334ym2qpdxwiyaahyybvpz93c8c9g4f9ipl";
+          name = "nix-env";
+          src = pkgs.fetchFromGitHub {
+            owner = "lilyball";
+            repo = "nix-env.fish";
+            rev = "7b65bd228429e852c8fdfa07601159130a818cfa";
+            sha256 = "069ybzdj29s320wzdyxqjhmpm9ir5815yx6n522adav0z2nz8vs4";
+          };
+        }
+        {
+          name = "thefuck";
+          src = pkgs.fetchFromGitHub
+            {
+              owner = "oh-my-fish";
+              repo = "plugin-thefuck";
+              rev = "6c9a926d045dc404a11854a645917b368f78fc4d";
+              sha256 = "1n6ibqcgsq1p8lblj334ym2qpdxwiyaahyybvpz93c8c9g4f9ipl";
+            };
+        }
+      ];
+
+      functions = {
+        gitignore = "curl -sL https://www.gitignore.io/api/$argv";
+      };
+
+      interactiveShellInit = ''
+        ${pkgs.thefuck}/bin/thefuck --alias | source
+
+        # Fish color
+        set -U fish_color_command 6CB6EB --bold
+        set -U fish_color_redirection DEB974
+        set -U fish_color_operator DEB974
+        set -U fish_color_end C071D8 --bold
+        set -U fish_color_error EC7279 --bold
+        set -U fish_color_param 6CB6EB
+        set fish_greeting
+      '';
+    };
+
+    # Fish prompt and style
+    starship = {
+      enable = true;
+      settings = {
+        add_newline = true;
+        command_timeout = 1000;
+        cmd_duration = {
+          format = " [$duration]($style) ";
+          style = "bold #EC7279";
+          show_notifications = true;
         };
-    }
-  ];
-
-  programs.fish.functions = {
-    gitignore = "curl -sL https://www.gitignore.io/api/$argv";
-  };
-
-  # Fish abbreviations
-  programs.fish.shellAbbrs = { };
-
-  # Fish alias : register alias command in fish
-  programs.fish.shellAliases = shellAliases;
-
-  programs.fish.interactiveShellInit = ''
-
-    ${pkgs.thefuck}/bin/thefuck --alias | source
-
-    # Fish color
-    set -U fish_color_command 6CB6EB --bold
-    set -U fish_color_redirection DEB974
-    set -U fish_color_operator DEB974
-    set -U fish_color_end C071D8 --bold
-    set -U fish_color_error EC7279 --bold
-    set -U fish_color_param 6CB6EB
-    set fish_greeting
-
-  '';
-
-  # jump like `z` or `fasd` 
-  programs.zoxide.enable = true;
-  programs.zoxide.enableBashIntegration = config.programs.bash.enable || config.programs.bashInteractive.enable;
-  programs.zoxide.enableZshIntegration = config.programs.zsh.enable;
-  programs.zoxide.enableFishIntegration = config.programs.fish.enable;
-
-  # Fish prompt and style
-  programs.starship.enable = true;
-  programs.starship.settings = {
-    add_newline = true;
-    command_timeout = 1000;
-    cmd_duration = {
-      format = " [$duration]($style) ";
-      style = "bold #EC7279";
-      show_notifications = true;
-    };
-    nix_shell = {
-      format = " [$symbol$state]($style) ";
-    };
-    battery = {
-      full_symbol = "🔋 ";
-      charging_symbol = "⚡️ ";
-      discharging_symbol = "💀 ";
-    };
-    git_branch = {
-      format = "[$symbol$branch]($style) ";
-    };
-    gcloud = {
-      format = "[$symbol$active]($style) ";
+        nix_shell = {
+          format = " [$symbol$state]($style) ";
+        };
+        battery = {
+          full_symbol = "🔋 ";
+          charging_symbol = "⚡️ ";
+          discharging_symbol = "💀 ";
+        };
+        git_branch = {
+          format = "[$symbol$branch]($style) ";
+        };
+        gcloud = {
+          format = "[$symbol$active]($style) ";
+        };
+      };
     };
   };
 }

ba620a53be4fa96317952f3bb634b5180a9163cd
r17x <ri7nz@evilfactory.id>
2022-07-02 09:35:26 +0700

    refactor(system): update system modules

--- system/bootstrap.nix
+++ system/bootstrap.nix
@@ -3,37 +3,40 @@
   # Nix configuration ------------------------------------------------------------------------------
 
   # Bootstrap
-  nix.binaryCaches = [
-    "https://cache.nixos.org/"
-    "https://r17.cachix.org/"
-  ];
+  nix = {
+    binaryCaches = [
+      "https://cache.nixos.org/"
+      # using cachix from r17
+      "https://r17.cachix.org/"
+    ];
 
-  nix.binaryCachePublicKeys = [
-    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-    "r17.cachix.org-1:vz0nG6BCbdgTPn7SEiOwe/3QwvjH1sb/VV9WLcBtkAY="
-  ];
+    binaryCachePublicKeys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      # using cachix from r17
+      "r17.cachix.org-1:vz0nG6BCbdgTPn7SEiOwe/3QwvjH1sb/VV9WLcBtkAY="
+    ];
 
-  nix.trustedUsers = [
-    "@admin"
-  ];
+    trustedUsers = [
+      "@admin"
+    ];
 
-  users.nix.configureBuildUsers = true;
+    # enable garbage-collection on weekly and delete-older-than 30 day
+    gc = {
+      automatic = true;
+      options = "--delete-older-than 30d";
+    };
 
-  # enable garbage-collection on weekly and delete-older-than 30 day
-  nix.gc = {
-    automatic = true;
-    options = "--delete-older-than 30d";
+    # this is configuration for /etc/nix/nix.conf
+    # so it will generated /etc/nix/nix.conf
+    extraOptions = ''
+      auto-optimise-store = true
+      experimental-features = nix-command flakes
+      keep-outputs = true
+      keep-derivations = true
+    '' + lib.optionalString (pkgs.system == "aarch64-darwin") ''
+      extra-platforms = x86_64-darwin aarch64-darwin
+    '';
   };
 
-  # Enable experimental nix command and flakes
-  # nix.package = pkgs.nixFlakes;
-  nix.extraOptions = ''
-    auto-optimise-store = true
-    experimental-features = nix-command flakes
-    keep-outputs = true
-    keep-derivations = true
-  '' + lib.optionalString (pkgs.system == "aarch64-darwin") ''
-    extra-platforms = x86_64-darwin aarch64-darwin
-  '';
+  users.nix.configureBuildUsers = true;
 }
-

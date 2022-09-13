ef2bc4edd0422057ac00d5a026361de4af8ea2ba
r17x <ri7nz@evilfactory.id>
2022-09-14 00:55:11 +0800

    feat(cache): add nix-community cachix

--- system/system.nix
+++ system/system.nix
@@ -14,6 +14,7 @@
 
       substituters = [
         "https://cache.nixos.org/"
+        "https://nix-community.cachix.org"
         # using cachix from r17
         "https://r17.cachix.org/"
       ];
@@ -21,6 +22,7 @@
 
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         # using cachix from r17
         "r17.cachix.org-1:vz0nG6BCbdgTPn7SEiOwe/3QwvjH1sb/VV9WLcBtkAY="
       ];

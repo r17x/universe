7410bb10765cde43241c85630fffc05aa5ddf015
r17x <ri7nz@evilfactory.id>
2022-09-16 14:41:04 +0800

    style: neovim.nix re-format

--- home/neovim.nix
+++ home/neovim.nix
@@ -4,26 +4,29 @@ let
   inherit (config.home.user-info) nixConfigDirectory;
 in
 {
-  programs.neovim.enable = true;
+  programs.neovim =
+    {
+      enable = true;
 
-  programs.neovim.extraConfig = ''
-    " -- apply all settings (option, global option, autocmds, & mappings)
-    lua require 'utils'.apply_settings(require 'settings')
-    " -- apply all plugins
-    lua require 'plugins'
-    set packpath^=~/.local/share/nvim/pack
-    set runtimepath^=~/.local/share/nvim
-  '';
+      extraConfig = ''
+        " -- apply all settings (option, global option, autocmds, & mappings)
+        lua require 'utils'.apply_settings(require 'settings')
+        " -- apply all plugins
+        lua require 'plugins'
+        set packpath^=~/.local/share/nvim/pack
+        set runtimepath^=~/.local/share/nvim
+      '';
 
+      extraPackages = with pkgs; [
+        rustPackages.rustc
+        rustPackages.rustfmt
+        rustPackages.cargo
+        ctags
+        tree-sitter
+        rnix-lsp
+      ];
+    };
+  # impure configurations
   xdg.configFile."nvim/lua".source = mkOutOfStoreSymlink "${nixConfigDirectory}/configs/nvim/lua";
   xdg.configFile."nvim/stylua.toml".source = mkOutOfStoreSymlink "${nixConfigDirectory}/configs/nvim/stylua.toml";
-
-  programs.neovim.extraPackages = with pkgs; [
-    rustPackages.rustc
-    rustPackages.rustfmt
-    rustPackages.cargo
-    ctags
-    tree-sitter
-    rnix-lsp
-  ];
 }

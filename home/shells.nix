22bc0c0238fa78aa8276df5fb38e6155f7cd7a94
r17x <ri7nz@evilfactory.id>
2023-01-08 20:08:08 +0700

    feat(shells): add comman nclean for remove GC nix

--- home/shells.nix
+++ home/shells.nix
@@ -5,6 +5,7 @@ let
   inherit (lib) mkAfter;
   shellAliases = with pkgs; {
     # Nix related
+    nclean = "nix-collect-garbage && nix-collect-garbage -d";
     drb = "darwin-rebuild build --flake ${nixConfigDirectory}";
     drs = "darwin-rebuild switch --flake ${nixConfigDirectory}";
     psc0 = "nix build ${nixConfigDirectory}#darwinConfigurations.RG.system --json | jq -r '.[].outputs | to_entries[].value' | cachix push r17";

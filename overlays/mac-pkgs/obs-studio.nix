b5d8563f43eaef18d6231a607ffefe7dc86ffca7
r17x <ri7nz@evilfactory.id>
2023-04-07 05:36:06 +0700

    feat(home): upgrade obs-studio to 29.0.2

--- overlays/mac-pkgs/obs-studio.nix
+++ overlays/mac-pkgs/obs-studio.nix
@@ -2,6 +2,7 @@
 , stdenv
 , fetchurl
 , undmg
+, unzip
 ,
 }:
 
@@ -12,12 +13,12 @@ let
   pname = "obs-studio";
 
   version = rec {
-    aarch64-darwin = "27.2.4";
+    aarch64-darwin = "29.0.2";
     x86_64-darwin = aarch64-darwin;
   }.${system} or throwSystem;
 
   sha256 = rec {
-    aarch64-darwin = "mu7ZKBbyP9tIGesbDJicFYqqskbgvQJJM0KWFLBkNfI=";
+    aarch64-darwin = "sha256-gJLdKUURT8AXwS0mcNl+elGKq0L0BfutJrwuInwaFWw=";
     x86_64-darwin = aarch64-darwin;
   }.${system} or throwSystem;
 
@@ -27,10 +28,12 @@ let
     in
     rec {
       aarch64-darwin = {
-        url = "${base}/obs-mac-${version}.dmg";
-        sha256 = sha256;
+        inherit sha256;
+        url = "${base}/obs-studio-${version}-macos-arm64.dmg";
       };
-      x86_64-darwin = aarch64-darwin;
+      x86_64-darwin = aarch64-darwin // {
+        url = "${base}/obs-studio-${version}-macos-x86_64.dmg";
+      };
     };
 
   src = fetchurl (srcs.${system} or throwSystem);
@@ -42,18 +45,43 @@ let
     platforms = [ "x86_64-darwin" "aarch64-darwin" ];
   };
 
+  appname = "OBS";
+
   darwin = stdenv.mkDerivation {
     inherit pname version src meta;
 
     nativeBuildInputs = [ undmg ];
+    buildInputs = [ unzip ];
+    unpackCmd = ''
+      echo "File to unpack: $curSrc"
+      if ! [[ "$curSrc" =~ \.dmg$ ]]; then return 1; fi
+      mnt=$(mktemp -d -t ci-XXXXXXXXXX)
 
-    sourceRoot = "OBS.app";
+      function finish {
+        echo "Detaching $mnt"
+        /usr/bin/hdiutil detach $mnt -force
+        rm -rf $mnt
+      }
+      trap finish EXIT
+
+      echo "Attaching $mnt"
+      /usr/bin/hdiutil attach -nobrowse -readonly $src -mountpoint $mnt
+
+      echo "What's in the mount dir"?
+      ls -la $mnt/
+
+      echo "Copying contents"
+      shopt -s extglob
+      DEST="$PWD"
+      (cd "$mnt"; cp -a !(Applications) "$DEST/")
+    '';
+    phases = [ "unpackPhase" "installPhase" ];
+
+    sourceRoot = "${appname}.app";
 
     installPhase = ''
-      runHook preInstall
-      mkdir -p $out/Applications/OBS.app
-      cp -R . $out/Applications/OBS.app
-      runHook postInstall
+      mkdir -p "$out/Applications/${appname}.app"
+      cp -a ./. "$out/Applications/${appname}.app/"
     '';
   };
 in

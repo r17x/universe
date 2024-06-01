de7aed5d68192d9d8b9c96a13b90ae68d2902673
r17x <ri7nz@evilfactory.id>
2024-06-01 21:48:21 +0700

    feat(aliases): git housekeeping aliases

--- nix/home/shells.nix
+++ nix/home/shells.nix
@@ -14,100 +14,134 @@ let
   #   done
   # '';
   commandFoldl' = builtins.foldl' (a: b: a + b + '' && '') '''';
-  shellAliases = {
-    tg = "tree --gitignore";
-    # Nix related
-    nclean = commandFoldl' [
-      "nix profile wipe-history"
-      "nix-collect-garbage"
-      "nix-collect-garbage -d"
-      "nix-collect-garbage --delete-old"
-      "nix store gc"
-      "nix store optimise"
-      "nix-store --verify --repair --check-contents"
-    ];
-    da = "direnv allow";
-    dr = "direnv reload";
-    drb = "darwin-rebuild build --flake ${nixConfigDirectory}";
-    drs = "darwin-rebuild switch --flake ${nixConfigDirectory}";
-    psc0 = "nix build ${nixConfigDirectory}#darwinConfigurations.RG.system --json | jq -r '.[].outputs | to_entries[].value' | cachix push r17";
-    psc1 = "nix build ${nixConfigDirectory}#darwinConfigurations.eR17.system --json | jq -r '.[].outputs | to_entries[].value' | cachix push r17";
+  shellAliases = with pkgs;
+    let
+      verify = writeScriptBin "verify" ''
+        [[ -z "$1" ]] && echo "No argument provided" && exit 0
 
-    # secret gpg export
-    gpbs = "gpg --export-options backup --export-secret-keys";
-    # public gpg export
-    gpbp = "gpg --export-options backup --export";
-    # secret or public gpg import
-    gprs = "gpg --export-options restore --import";
-    # gpg trust data
-    gpbt = "gpg --export-ownertrust";
-    gprt = "gpg --import-ownertrust";
+        [[ $1 == ^-?[0-9]+(\.[0-9]+)?$ ]] && echo "The first argument is not a number: $1" && exit 0
+      '';
+      cmd = a: b: x: ''
+        set -e 
+        ${verify}/bin/verify $1 || exit 0
+        ${git}/bin/git for-each-ref --sort=${a} --format '%(refname:short) %(${a}:format:%s)' "${b}" | while read tag tagdate; do
+          threshold_date=$(date -d "$1 days ago" --utc '+%s')
+          if [ -n "$tagdate" ]; then
+            if [ "$tagdate" -lt "$threshold_date" ]; then
+              echo "==> $tag is older than $1 days"
+              echo "==> $tag  will be deleted"
+              TAGS="$TAGS $tag"
+            fi
+          fi
+        done
+        ${x} $TAGS
+      '';
+      scripts = {
+        gdb = writeScriptBin "gdb" (cmd "committerdate" "refs/heads" "git branch -D");
+        gdbr = writeScriptBin "gdbr" (cmd "committerdate" "origin/refs/heads" "git push origin -d");
+        gdt = writeScriptBin "gdt" (cmd "taggerdate" "refs/tags/*" "git tag -d");
+        gdtr = writeScriptBin "gdtr" (cmd "taggerdate" "origin/refs/tags/*" "git push origin -d");
+      };
+    in
+    {
+      gdb = ''${scripts.gdb}/bin/gdb'';
+      gdbr = ''${scripts.gdbr}/bin/gdbr'';
+      gdt = ''${scripts.gdt}/bin/gdt'';
+      gdtr = ''${scripts.gdtr}/bin/gdtr'';
 
-    # lenv show list generations aka list build version
-    # senv switch generation <number>
-    # denv delete generation <number>
-    # renv rollback to previous version number
-    # param: <GENEREATION_NUMBER> 
-    # run lenv before if you want to see <GENEREATION_NUMBER>
-    lenv = "nix-env --list-generations";
-    senv = "nix-env --switch-generation";
-    denv = "nix-env --delete-generations";
-    doenv = "denv old";
-    renv = "nix-env --rollback";
-    # is equivalent to: nix build --recreate-lock-file
-    flakeup-all = "nix flake update ${nixConfigDirectory}";
-    # example: 
-    # $ flakeup home-manager
-    flakeup = "nix flake lock ${nixConfigDirectory} --update-input";
-    nb = "nix build";
-    ndp = "nix develop";
-    nf = "nix flake";
-    nr = "nix run";
-    ns = "nix-shell";
-    nq = "nix search";
-    # Cryptography
-    age = "${pkgs.rage}/bin/rage";
+      tg = "tree --gitignore";
+      # Nix related
+      nclean = commandFoldl' [
+        "nix profile wipe-history"
+        "nix-collect-garbage"
+        "nix-collect-garbage -d"
+        "nix-collect-garbage --delete-old"
+        "nix store gc"
+        "nix store optimise"
+        "nix-store --verify --repair --check-contents"
+      ];
+      da = "direnv allow";
+      dr = "direnv reload";
+      drb = "darwin-rebuild build --flake ${nixConfigDirectory}";
+      drs = "darwin-rebuild switch --flake ${nixConfigDirectory}";
+      psc0 = "nix build ${nixConfigDirectory}#darwinConfigurations.RG.system --json | jq -r '.[].outputs | to_entries[].value' | cachix push r17";
+      psc1 = "nix build ${nixConfigDirectory}#darwinConfigurations.eR17.system --json | jq -r '.[].outputs | to_entries[].value' | cachix push r17";
 
-    # Shell related
-    e = "nvim";
-    grep = "${pkgs.ripgrep}/bin/rg";
-    c = "z";
-    cc = "zi";
-    # Others
-    p = "ping";
-    l = "ls -l";
-    la = "ls -a";
-    lla = "ls -la";
-    lt = "ls --tree";
-    cat = "${pkgs.bat}/bin/bat";
-    du = "${pkgs.du-dust}/bin/dust";
-    git = "${pkgs.git}/bin/git";
-    pullhead = "git pull origin (git rev-parse --abbrev-ref HEAD)";
-    beda = "gd";
-    ingfo = "git status";
-    tarek = "pullhead";
-    pushhead = "git push origin (git rev-parse --abbrev-ref HEAD)";
-    gas = "pushhead";
-    gasin = "pushhead --force";
-    gi = "gitignore";
-    g = "git";
-    gtemp = "git commit -m \"temp\" --no-verify";
-    gf = "git flow";
-    gl = "git log --graph --oneline --all";
-    gll = "git log --oneline --decorate --all --graph --stat";
-    gld = "git log --oneline --all --pretty=format:\"%h%x09%an%x09%ad%x09%s\"";
-    gls = "gl --show-signature";
-    gfa = "git fetch --all";
-    grc = "git rebase --continue";
-    rm = "rm -i";
+      # secret gpg export
+      gpbs = "gpg --export-options backup --export-secret-keys";
+      # public gpg export
+      gpbp = "gpg --export-options backup --export";
+      # secret or public gpg import
+      gprs = "gpg --export-options restore --import";
+      # gpg trust data
+      gpbt = "gpg --export-ownertrust";
+      gprt = "gpg --import-ownertrust";
 
-    todo = "nvim ${nixConfigDirectory}/notes/todo.norg";
-    todox = "nvim ${nixConfigDirectory}/secrets/todo.norg";
+      # lenv show list generations aka list build version
+      # senv switch generation <number>
+      # denv delete generation <number>
+      # renv rollback to previous version number
+      # param: <GENEREATION_NUMBER> 
+      # run lenv before if you want to see <GENEREATION_NUMBER>
+      lenv = "nix-env --list-generations";
+      senv = "nix-env --switch-generation";
+      denv = "nix-env --delete-generations";
+      doenv = "denv old";
+      renv = "nix-env --rollback";
+      # is equivalent to: nix build --recreate-lock-file
+      flakeup-all = "nix flake update ${nixConfigDirectory}";
+      # example: 
+      # $ flakeup home-manager
+      flakeup = "nix flake lock ${nixConfigDirectory} --update-input";
+      nb = "nix build";
+      ndp = "nix develop";
+      nf = "nix flake";
+      nr = "nix run";
+      ns = "nix-shell";
+      nq = "nix search";
+      # Cryptography
+      age = "${pkgs.rage}/bin/rage";
 
-    # Development
-    # docker = "${pkgs.podman}/bin/podman";
-    # docker-compose = "${pkgs.podman-compose}/bin/podman-compose";
-  };
+      # Shell related
+      e = "nvim";
+      grep = "${pkgs.ripgrep}/bin/rg";
+      c = "z";
+      cc = "zi";
+      # Others
+      p = "ping";
+      l = "ls -l";
+      la = "ls -a";
+      lla = "ls -la";
+      lt = "ls --tree";
+      cat = "${pkgs.bat}/bin/bat";
+      du = "${pkgs.du-dust}/bin/dust";
+      git = "${pkgs.git}/bin/git";
+      pullhead = "git pull origin (git rev-parse --abbrev-ref HEAD)";
+      beda = "gd";
+      ingfo = "git status";
+      tarek = "pullhead";
+      pushhead = "git push origin (git rev-parse --abbrev-ref HEAD)";
+      gas = "pushhead";
+      gasin = "pushhead --force";
+      gi = "gitignore";
+      g = "git";
+      gtemp = "git commit -m \"temp\" --no-verify";
+      gf = "git flow";
+      gl = "git log --graph --oneline --all";
+      gll = "git log --oneline --decorate --all --graph --stat";
+      gld = "git log --oneline --all --pretty=format:\"%h%x09%an%x09%ad%x09%s\"";
+      gls = "gl --show-signature";
+      gfa = "git fetch --all";
+      grc = "git rebase --continue";
+      rm = "rm -i";
+
+      todo = "nvim ${nixConfigDirectory}/notes/todo.norg";
+      todox = "nvim ${nixConfigDirectory}/secrets/todo.norg";
+
+      # Development
+      # docker = "${pkgs.podman}/bin/podman";
+      # docker-compose = "${pkgs.podman-compose}/bin/podman-compose";
+    };
 in
 {
   home = {

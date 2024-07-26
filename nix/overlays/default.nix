89745ea6c44fa02917a0f91289962fa4cb6e983d
r17x <ri7nz@evilfactory.id>
2024-07-26 11:46:13 +0700

    refactor(darwin): update font definitions and add overlays

--- nix/overlays/default.nix
+++ nix/overlays/default.nix
@@ -9,6 +9,24 @@
   flake.overlays.default =
     final: prev:
     {
+      sf-mono-liga-bin = prev.stdenvNoCC.mkDerivation (finalAttrs: {
+        pname = "sf-mono-liga-bin";
+        version = "7723040ef50633da5094f01f93b96dae5e9b9299";
+
+        src = prev.fetchFromGitHub {
+          owner = "shaunsingh";
+          repo = "SFMono-Nerd-Font-Ligaturized";
+          rev = finalAttrs.version;
+          sha256 = "sha256-vPUl6O/ji4hHIH7/qSbUe7q1QdugE1D1ZRw92QcSSDQ=";
+        };
+
+        dontConfigure = true;
+        installPhase = ''
+          mkdir -p $out/share/fonts/opentype
+          cp -R $src/*.otf $out/share/fonts/opentype
+        '';
+      });
+
       nixfmt = prev.nixfmt-rfc-style;
       sketchybar-app-font = prev.stdenv.mkDerivation {
         name = "sketchybar-app-font";

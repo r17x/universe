9994dac4e7f4bddda331add931c2d8eebcd6ee03
r17x <ri7nz@evilfactory.id>
2024-12-08 06:22:11 +0700

    feat(modules): add aerospaces in mouseless

--- nix/nixosModules/darwin/mouseless.nix
+++ nix/nixosModules/darwin/mouseless.nix
@@ -9,7 +9,13 @@
 let
   cfg = config.mouseless;
 
-  inherit (lib) mdDoc mkEnableOption mkIf;
+  inherit (lib)
+    mdDoc
+    mkEnableOption
+    mkIf
+    mkOption
+    types
+    ;
 
   lua = pkgs.lua54Packages.lua.withPackages (ps: [
     ps.lua
@@ -21,7 +27,15 @@ in
 {
   options.mouseless.enable = mkEnableOption (mdDoc "Enable Mouseless with yabai skhd and sketchybar");
 
-  config = mkIf (cfg.enable) {
+  options.mouseless.wm = mkOption {
+    type = types.enum [
+      "yabai"
+      "aerospace"
+    ];
+    default = "yabai";
+  };
+
+  config = mkIf cfg.enable {
     services.sketchybar = {
       inherit (cfg) enable;
       package = branches.master.sketchybar;
@@ -38,8 +52,58 @@ in
         '';
     };
 
+    services.aerospace = {
+      enable = cfg.wm == "aerospace";
+      settings = {
+        exec-on-workspace-change = [
+          "/bin/bash"
+          "-c"
+          "${lib.getExe pkgs.sketchybar} --trigger space_workspace_change FOCUSED=$AEROSPACE_FOCUSED_WORKSPACE"
+        ];
+        gaps = {
+          outer.top = 50;
+          outer.bottom = 15;
+          outer.left = 15;
+          outer.right = 15;
+          inner.horizontal = 15;
+          inner.vertical = 15;
+        };
+        mode.main.binding = {
+          alt-space = "layout floating";
+          alt-z = "resize smart +10";
+          alt-shift-z = "resize smart -10";
+          alt-v = "layout v_tiles";
+          alt-shift-v = "layout h_tiles";
+          alt-h = "focus left";
+          alt-j = "focus down";
+          alt-k = "focus up";
+          alt-l = "focus right";
+          alt-f = "fullscreen";
+          alt-shift-space = "balance-sizes";
+          alt-1 = "workspace 1";
+          alt-2 = "workspace 2";
+          alt-3 = "workspace 3";
+          alt-4 = "workspace 4";
+          alt-5 = "workspace 5";
+          alt-6 = "workspace 6";
+          alt-7 = "workspace 7";
+          alt-8 = "workspace 8";
+          alt-9 = "workspace 9";
+          alt-shift-1 = "move-node-to-workspace 1";
+          alt-shift-2 = "move-node-to-workspace 2";
+          alt-shift-3 = "move-node-to-workspace 3";
+          alt-shift-4 = "move-node-to-workspace 4";
+          alt-shift-5 = "move-node-to-workspace 5";
+          alt-shift-6 = "move-node-to-workspace 6";
+          alt-shift-7 = "move-node-to-workspace 7";
+          alt-shift-8 = "move-node-to-workspace 8";
+          alt-shift-9 = "move-node-to-workspace 9";
+        };
+      };
+    };
+
     services.yabai = {
-      inherit (cfg) enable;
+      enable = cfg.wm == "yabai";
       package = branches.master.yabai;
       enableScriptingAddition = false; # true when SIP Disabled
       config = {
@@ -103,7 +167,7 @@ in
     ];
 
     services.skhd = {
-      enable = cfg.enable;
+      enable = cfg.wm == "yabai";
       package = branches.master.skhd;
       skhdConfig =
         let

bc4b4638550c17da5ddee16d43b54cfdeb59f29d
r17x <ri7nz@evilfactory.id>
2025-01-04 02:20:35 +0700

    feat!(inputs): update flake-parts nixpkgs-unstable nix-darwin home-manager nixd

--- nix/nixosModules/darwin/packages.nix
+++ nix/nixosModules/darwin/packages.nix
@@ -1,5 +1,5 @@
 # I have been start to use fully in nix at 9-Feb-2022
-# and found how to create flake, home-manager, and darwin in nix 
+# and found how to create flake, home-manager, and darwin in nix
 # Here: https://gist.github.com/jmatsushita/5c50ef14b4b96cb24ae5268dab613050
 
 {
@@ -33,14 +33,10 @@
     sf-mono-liga-bin
     sf-symbols-font
 
-    # name of nerdfonts see {https://github.com/NixOS/nixpkgs/blob/nixos-24.05/pkgs/data/fonts/nerdfonts/shas.nix}
-    (nerdfonts.override {
-      fonts = [
-        "JetBrainsMono"
-        "FiraCode"
-        "Hack"
-        "NerdFontsSymbolsOnly"
-      ];
-    })
+    # name of nerdfonts see {https://github.com/NixOS/nixpkgs/blob/nixos-24.11/pkgs/data/fonts/nerdfonts/shas.nix}
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.fira-code
+    nerd-fonts.hack
+    nerd-fonts.symbols-only
   ];
 }

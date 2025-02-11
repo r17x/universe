666cc39cc154fdc5644bb376395090a909ad78b7
r17x <ri7nz@evilfactory.id>
2025-02-11 23:12:06 +0700

    feat(home): add gh-dash

--- nix/home/git.nix
+++ nix/home/git.nix
@@ -18,98 +18,106 @@ let
   };
 in
 {
-  programs.jujutsu = {
-    enable = true;
-    settings = {
-      user = lib.removeAttrs evil [ "signingKey" ];
-    };
-  };
 
-  programs.git = {
-    enable = true;
+  home.packages = [ pkgs.git-filter-repo ];
+  home.shellAliases.ghd = "gh-dash";
 
-    aliases = {
-      a = "add";
-      c = "clone";
-      cfd = "clean -fd";
-      ca = "commit --amend";
-      can = "commit --amend --no-edit";
-      r = "rebase";
-      ro = "rebase origin/master";
-      rc = "rebase --continue";
-      ra = "rebase --abort";
-      ri = "rebase -i";
-      # need to install vim-conflicted
-      res = "!nvim +Conflicted";
-      # use for resolve conflicted
-      # accept-ours
-      aco = "!f() { git checkout --ours -- \"\${@:-.}\"; git add -u \"\${@:-.}\"; }; f";
-      # accept-theirs
-      ace = "!f() { git checkout --theirs -- \"\${@:-.}\"; git add -u \"\${@:-.}\"; }; f";
-      branches = "branch --sort=-committerdate --format='%(HEAD)%(color:yellow) %(refname:short) | %(color:bold red)%(committername) | %(color:bold green)%(committerdate:relative) | %(color:blue)%(subject)%(color:reset)' --color=always";
-      bs = "branches";
-      fa = "fetch --all";
-    };
-
-    extraConfig = {
-      gpg.program = "gpg";
-      rerere.enable = true;
-      commit.gpgSign = true;
-      pull.ff = "only";
-      diff.tool = "vimdiff";
-      difftool.prompt = false;
-      merge.tool = "vimdiff";
-      url = {
-        "git@gitlab.com:" = {
-          insteadOf = "https://gitlab.com/";
-        };
-        "git@bitbucket.org:" = {
-          insteadOf = "https://bitbucket.org/";
-        };
+  programs = {
+    jujutsu = {
+      enable = true;
+      settings = {
+        user = lib.removeAttrs evil [ "signingKey" ];
       };
     };
 
-    includes = [
-      {
-        condition = "gitdir:~/w0/";
-        contents.user = work;
-      }
+    ### git tools
+    ## github cli
+    gh = {
+      enable = true;
+      settings.git_protocol = "ssh";
+      settings.aliases = {
+        co = "pr checkout";
+        pv = "pr view";
+      };
+    };
 
-      {
-        condition = "gitdir:~/w1/";
-        contents.user = w1;
-      }
+    gh-dash.enable = true;
 
-      {
-        condition = "gitdir:~/go/";
-        contents.user = w1;
-      }
+    git = {
+      enable = true;
 
-      {
-        condition = "gitdir:~/evl/";
-        contents.user = evil;
-      }
+      aliases = {
+        a = "add";
+        c = "clone";
+        cfd = "clean -fd";
+        ca = "commit --amend";
+        can = "commit --amend --no-edit";
+        r = "rebase";
+        ro = "rebase origin/master";
+        rc = "rebase --continue";
+        ra = "rebase --abort";
+        ri = "rebase -i";
+        # need to install vim-conflicted
+        res = "!nvim +Conflicted";
+        # use for resolve conflicted
+        # accept-ours
+        aco = "!f() { git checkout --ours -- \"\${@:-.}\"; git add -u \"\${@:-.}\"; }; f";
+        # accept-theirs
+        ace = "!f() { git checkout --theirs -- \"\${@:-.}\"; git add -u \"\${@:-.}\"; }; f";
+        branches = "branch --sort=-committerdate --format='%(HEAD)%(color:yellow) %(refname:short) | %(color:bold red)%(committername) | %(color:bold green)%(committerdate:relative) | %(color:blue)%(subject)%(color:reset)' --color=always";
+        bs = "branches";
+        fa = "fetch --all";
+      };
 
-      {
-        condition = "gitdir:~/.local/share/";
-        contents.user = evil;
-      }
+      extraConfig = {
+        gpg.program = "gpg";
+        rerere.enable = true;
+        commit.gpgSign = true;
+        pull.ff = "only";
+        diff.tool = "vimdiff";
+        difftool.prompt = false;
+        merge.tool = "vimdiff";
+        url = {
+          "git@gitlab.com:" = {
+            insteadOf = "https://gitlab.com/";
+          };
+          "git@bitbucket.org:" = {
+            insteadOf = "https://bitbucket.org/";
+          };
+        };
+      };
 
-      {
-        condition = "gitdir:~/.config/nixpkgs/";
-        contents.user = evil;
-      }
-    ];
+      includes = [
+        {
+          condition = "gitdir:~/w0/";
+          contents.user = work;
+        }
+
+        {
+          condition = "gitdir:~/w1/";
+          contents.user = w1;
+        }
+
+        {
+          condition = "gitdir:~/go/";
+          contents.user = w1;
+        }
+
+        {
+          condition = "gitdir:~/evl/";
+          contents.user = evil;
+        }
+
+        {
+          condition = "gitdir:~/.local/share/";
+          contents.user = evil;
+        }
+
+        {
+          condition = "gitdir:~/.config/nixpkgs/";
+          contents.user = evil;
+        }
+      ];
+    };
   };
-
-  ### git tools
-  ## github cli
-  programs.gh.enable = true;
-  programs.gh.settings.git_protocol = "ssh";
-  programs.gh.settings.aliases = {
-    co = "pr checkout";
-    pv = "pr view";
-  };
-
-  home.packages = [ pkgs.git-filter-repo ];
 }
